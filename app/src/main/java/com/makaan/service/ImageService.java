ad7c07ba9b149406b46d4a291428f5098a4c2a55
adlakha.vaibhav <vaibhav@proptiger.com>
2016-01-17 19:28:49 +0530

    listing detail image event

--- app/src/main/java/com/makaan/service/ImageService.java
+++ app/src/main/java/com/makaan/service/ImageService.java
@@ -2,9 +2,11 @@ package com.makaan.service;
 
 import com.google.gson.reflect.TypeToken;
 import com.makaan.constants.ApiConstants;
+import com.makaan.event.listing.ListingImagesGetEvent;
 import com.makaan.network.MakaanNetworkClient;
 import com.makaan.network.ObjectGetCallback;
 import com.makaan.response.listing.detail.ListingDetailImage;
+import com.makaan.util.AppBus;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -12,12 +14,12 @@ import java.util.ArrayList;
 /**
  * Created by vaibhav on 17/01/16.
  */
-public class ImageService implements  MakaanService {
+public class ImageService implements MakaanService {
 
 
-    public void getListingImages(Long listingId){
+    public void getListingImages(final Long listingId) {
 
-        if(null != listingId) {
+        if (null != listingId) {
             Type listingDetailImageList = new TypeToken<ArrayList<ListingDetailImage>>() {
             }.getType();
             StringBuilder detailImageListUrl = new StringBuilder(ApiConstants.LISTING_IMAGE);
@@ -27,9 +29,12 @@ public class ImageService implements  MakaanService {
                 @Override
                 public void onSuccess(Object responseObject) {
                     ArrayList<ListingDetailImage> detailImages = (ArrayList<ListingDetailImage>) responseObject;
-                    //TODO: raise event
+                    ListingImagesGetEvent listingImagesGetEvent = new ListingImagesGetEvent();
+                    listingImagesGetEvent.listingId = listingId;
+                    listingImagesGetEvent.listingDetailImages = detailImages;
+                    AppBus.getInstance().post(listingImagesGetEvent);
                 }
-            },true);
+            }, true);
         }
 
     }
