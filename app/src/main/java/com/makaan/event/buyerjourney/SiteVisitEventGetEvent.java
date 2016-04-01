a33a4bacb4f6fe964761d3d4cb0e13b296bee895
Aishwarya Pratap Singh <aishwarya@PT-MAC-27.local>
2016-04-02 00:05:41 +0530

    handeling site visit error

---
+++ app/src/main/java/com/makaan/event/buyerjourney/SiteVisitEventGetEvent.java
@@ -0,0 +1,10 @@
+package com.makaan.event.buyerjourney;
+
+import com.makaan.event.MakaanEvent;
+
+/**
+ * Created by aishwarya on 01/04/16.
+ */
+public class SiteVisitEventGetEvent extends MakaanEvent {
+    public boolean isScheduled;
+}
